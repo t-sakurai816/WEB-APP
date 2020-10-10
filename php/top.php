2e37b9edef8ed023552c977f85d1382bc18be984
t-sakurai816 <poppun_0816@icloud.com>
2020-10-10 18:10:42 +0900

    修正
    25行目をgetからpostに変更
    $_GET[]に""がついていなかったので、変更

--- php/top.php
+++ php/top.php
@@ -5,24 +5,24 @@
 </head>
 <body bgcolor="#FFDDDD">
 <php
-if(isset($_GET[win])){  //勝った回数
-   $win = $_GET[win];
+if(isset($_GET["win"])){  //勝った回数
+   $win = $_GET["win"];
    } else {
    $win = 0;
    }
-if(isset($_GET['lose'])){  //負けた回数
-   $lose = $_GET['lose']
+if(isset($_GET["lose"])){  //負けた回数
+   $lose = $_GET["lose"]
    } else {
    $lose = 0;
-　 }
-if(isset($_GET['aiko'])){  //あいこだった回数
-   $aiko = $_GET['aiko'];
+   }
+if(isset($_GET["aiko"])){  //あいこだった回数
+   $aiko = $_GET["aiko"];
    } else {
    $aiko = 0;
    }
 ?>
 
-<form action="result.php" method="get">
+<form action="result.php" method="post">
 じゃんけん　<input type="radio" name="janken" value="0">グー
 <input type="radio" name="janken" value="1">チョキ
 <input type="radio" name="janken" value="2">パー
