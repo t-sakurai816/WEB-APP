1e9dc0b3a8b715ec5928226dd1884b15ec37b391
t-sakurai816 <poppun_0816@icloud.com>
2020-10-10 18:19:56 +0900

    Update top.php
    字下げを調整

--- php/top.php
+++ php/top.php
@@ -1,37 +1,37 @@
 ﻿<html>
-<head lang="ja">
-<meta http-equiv="Content-Type" content="text/html; charset=utf8">
-<title>シンプルなじゃんけんゲーム</title>
+   <head lang="ja">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+   <title>シンプルなじゃんけんゲーム</title>
 </head>
-<body bgcolor="#FFDDDD">
-<php
-if(isset($_GET["win"])){  //勝った回数
-   $win = $_GET["win"];
-   } else {
-   $win = 0;
-   }
-if(isset($_GET["lose"])){  //負けた回数
-   $lose = $_GET["lose"]
-   } else {
-   $lose = 0;
-   }
-if(isset($_GET["aiko"])){  //あいこだった回数
-   $aiko = $_GET["aiko"];
-   } else {
-   $aiko = 0;
-   }
-?>
+   <body bgcolor="#FFDDDD">
+      <php
+      if(isset($_GET["win"])){  //勝った回数
+         $win = $_GET["win"];
+         } else {
+         $win = 0;
+         }
+      if(isset($_GET["lose"])){  //負けた回数
+         $lose = $_GET["lose"]
+         } else {
+         $lose = 0;
+         }
+      if(isset($_GET["aiko"])){  //あいこだった回数
+         $aiko = $_GET["aiko"];
+         } else {
+         $aiko = 0;
+         }
+      ?>
 
-<form action="result.php" method="get">
-じゃんけん 
-   <input type="radio" name="janken" value="0">グー <!--ラジオボタン-->
-   <input type="radio" name="janken" value="1">チョキ <!--ラジオボタン-->
-   <input type="radio" name="janken" value="2">パー <!--ラジオボタン-->
-   <input type="submit" value="ぽん"> <!--送信-->
+      <form action="result.php" method="get">
+      じゃんけん 
+         <input type="radio" name="janken" value="0">グー <!--ラジオボタン-->
+         <input type="radio" name="janken" value="1">チョキ <!--ラジオボタン-->
+         <input type="radio" name="janken" value="2">パー <!--ラジオボタン-->
+         <input type="submit" value="ぽん"> <!--送信-->
 
-   <input  value=<?php echo $win; ?>  name="win">
-   <input  value=<?php echo $aik0; ?> name="aiko">
-   <input  value=<?php echo $lose; ?>  name="lose">
-</form>
-</body>
+         <input  value=<?php echo $win; ?>  name="win">
+         <input  value=<?php echo $aik0; ?> name="aiko">
+         <input  value=<?php echo $lose; ?>  name="lose">
+      </form>
+   </body>
 </html>
