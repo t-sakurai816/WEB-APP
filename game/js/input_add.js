8d873715997104302f7f452d3ea8293aef86526c
t-sakurai816 <poppun_0816@icloud.com>
2020-11-03 18:40:06 +0900

    Create input_add.js

---
+++ game/js/input_add.js
@@ -0,0 +1,3 @@
+const input_add = (a,b, c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,aa,ab,ac,ad,ae,af,ag,ah,ai,aj,ak,al,am,an,ao,ap,aq,ar,at,as,at) => {
+  return (a && b &&  c && d && e && f && g && h && i && j && k && l && m && n && o && p && q && r && s && t && u && v && w && x && y && z && aa && ab && ac && ad && ae && af && ag && ah && ai && aj && ak && al && am && an && ao && ap && aq && ar && at && as && at)/*&& parseInt(x)+parseInt(y)*/;
+}
