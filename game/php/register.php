d40eefbbedfab496a6b5e8dbd537202cad0b7a62
t-sakurai816 <poppun_0816@icloud.com>
2020-11-06 00:00:07 +0900

    一旦コメントアウト

--- game/php/register.php
+++ game/php/register.php
@@ -17,25 +17,25 @@ try {
   $msg = $e->getMessage();
 }
 
-//フォームに入力されたnameがすでに登録されていないかチェック
-$sql = "SELECT * FROM users WHERE name = :name";
-$stmt = $dbh->prepare($sql);
-$stmt->bindValue(':name', $mail);
-$stmt->execute();
-$member = $stmt->fetch();
-if ($member['name'] === $mail) {
-    $msg = '同じNameが存在します。別の名前に変更してください。';
-    $link = '<a href="../index.html">戻る</a>';
-} else {
-    //登録されていなければinsert 
-    $sql = "INSERT INTO users(name, pass) VALUES (:name, :pass)";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindValue(':name', $name);
-    $stmt->bindValue(':pass', $pass);
-    $stmt->execute();
-    $msg = '会員登録が完了しました';
-    $link = '<a href="select.html">いざ！ルーレット！</a>';
-}
+// //フォームに入力されたnameがすでに登録されていないかチェック
+// $sql = "SELECT * FROM users WHERE name = :name";
+// $stmt = $dbh->prepare($sql);
+// $stmt->bindValue(':name', $mail);
+// $stmt->execute();
+// $member = $stmt->fetch();
+// if ($member['name'] === $mail) {
+//     $msg = '同じNameが存在します。別の名前に変更してください。';
+//     $link = '<a href="../index.html">戻る</a>';
+// } else {
+//     //登録されていなければinsert 
+//     $sql = "INSERT INTO users(name, pass) VALUES (:name, :pass)";
+//     $stmt = $dbh->prepare($sql);
+//     $stmt->bindValue(':name', $name);
+//     $stmt->bindValue(':pass', $pass);
+//     $stmt->execute();
+//     $msg = '会員登録が完了しました';
+//     $link = '<a href="select.html">いざ！ルーレット！</a>';
+// }
 ?>
 
 <h1><?php echo $msg; ?></h1><!--メッセージの出力-->
