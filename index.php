27106baa42e06eb1a98ac633c93cddf89756dc5e
t-sakurai816 <poppun_0816@icloud.com>
2020-11-06 20:28:31 +0900

    add link

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
       <div>
         <h2>Other Pages</h2>
         <li><a href="./html/position.html">Get Position</a></li>
+        <li><a href="https://webapp.massyu.net/game/index.html">ルーレットゲーム</a></li>
         <hr class="cp_hr02" /><!--破線-->
         <li><a href="./php/sample.php">CIT Pic (lesson1)</a></li>
         <li><a href="./php/top.php">top.php (lesson2)</a></li>
