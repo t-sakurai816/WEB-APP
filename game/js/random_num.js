8c4d43e49f1335dcc5cb790e09c287232772f398
t-sakurai816 <poppun_0816@icloud.com>
2020-11-07 23:53:22 +0900

    Delete random_num.js

--- game/js/random_num.js
+++
@@ -1,7 +0,0 @@
-getRandomInt = (min, max) =>{
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  console.log(Math.floor(Math.random() * (max - min)) + min); //The maximum is exclusive and the minimum is inclusive
-}
-
-// getRandomInt(10000,100000);
