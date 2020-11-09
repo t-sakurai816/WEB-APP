e4ee3694203befa90a357892d542bec0e33b2275
t-sakurai816 <poppun_0816@icloud.com>
2020-11-09 14:13:00 +0900

    Update result.php

--- game/result.php
+++ game/result.php
@@ -4,8 +4,27 @@ session_start();
 // DB情報（elastic beanstalkの環境変数から読み込む）
 require_once('php/db-info.php');
 
-//MySQLからBETされた枚数を引く
-require_once('php/sub-db.php');
+function Subtraction($bet_money, $id){//DBからgoldの値を引くプログラム
+
+  // DB情報（elastic beanstalkの環境変数から読み込む）
+  // require_once('../php/db-config.php');
+  $dsn = "mysql:host=$host; dbname=$dbname; charset=utf8";
+
+  try {
+    $dbh = new PDO($dsn, $username, $password);
+    // echo "接続成功";
+    $sql = "UPDATE roulette SET gold = gold - :money WHERE id = :id";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':money', $money, PDO::PARAM_INT);
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+  } catch (PDOException $e) {
+    // エラーのときエラーメッセージ
+    $msg = $e->getMessage();
+    alert('$msg');
+  }
+}
 
 
 
