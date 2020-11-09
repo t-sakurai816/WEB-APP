a60cfe8d853b0c25498859def1b3dc735b94686c
t-sakurai816 <poppun_0816@icloud.com>
2020-11-09 10:05:51 +0900

    Create fetch.js

---
+++ game/js/fetch.js
@@ -0,0 +1,17 @@
+const data = { username: 'example' };
+
+fetch('https://webapp.massyu.net/php/test.php', {
+  method: 'POST', // or 'PUT'
+  mode: 'cors',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
