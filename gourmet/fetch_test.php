2ccbbfd73c702e20bc27d39a1efa098b89810a9a
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2020-12-09 17:37:21 +0900

    update

--- gourmet/fetch_test.php
+++ gourmet/fetch_test.php
@@ -1,2 +1,20 @@
-<?php
-echo $_POST['firstName']; 
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>test</title>
+  <script src="fetch_test.js"></script>
+</head>
+
+<body>
+  <div class="Container">
+    <button class="btn" id="start">start</button>
+  </div>
+
+  <?php
+echo $_POST['firstName']; ?>
+</body>
+
+</html>
