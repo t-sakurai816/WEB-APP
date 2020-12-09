08c9a027f74088f46f853b58b32613a5b90f885a
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2020-12-09 19:02:14 +0900

    Update fetch_test.php

--- gourmet/fetch_test.php
+++ gourmet/fetch_test.php
@@ -10,6 +10,23 @@
 
 <body>
   <div class="Container">
+    <script>
+      const start = document.getElementById('start');
+      start.addEventListener('click', () => {
+        const postData = new FormData; // フォーム方式で送る場合
+        postData.set('firstName', 'hoge'); // set()で格納する
+        postData.set('lastName', 'fuga');
+
+        const data = {
+          method: 'POST',
+          body: postData
+        };
+
+        fetch('fetch_test.php', data)
+          .then((res) => res.text())
+          .then(console.log);
+      });
+    </script>
     <button class="btn" id="start">start</button>
   </div>
 
