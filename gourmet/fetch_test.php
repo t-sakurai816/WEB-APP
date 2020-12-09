69e34f98aed5dc4624d50801e74ccec09ba135bf
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2020-12-09 19:06:52 +0900

    Update fetch_test.php

--- gourmet/fetch_test.php
+++ gourmet/fetch_test.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>test</title>
-  <script src="fetch_test.js"></script>
+  <!-- <script src="fetch_test.js"></script> -->
 </head>
 
 <body>
